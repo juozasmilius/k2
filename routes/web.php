d76cbfe8a579aa4a7f68b98b1a257a8f0f1a04dd
Juozas Milius <juozasmilius@gmail.com>
2018-11-15 22:52:49 +0200

    Added dashboard and landingPage route

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landingPage');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'DashboardController@index');
